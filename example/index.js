b218467193ee10cdae4e44b4b7b2de8a0e8477e8
Sandy Barnabas <sbarnabas@gmail.com>
2016-05-08 04:20:34 -0400

    hackathon changes

--- example/index.js
+++ example/index.js
@@ -3,9 +3,9 @@ const player = AVS.Player;
 
 const avs = new AVS({
   debug: true,
-  clientId: 'amzn1.application-oa2-client.696ab90fc5844fdbb8efc17394a79c00',
-  deviceId: 'test_device',
-  deviceSerialNumber: 123,
+  clientId: 'amzn1.application-oa2-client.8871610ede8d4d91ac26122569319ed7',
+  deviceId: 'gastro_club',
+  deviceSerialNumber: 123456,
   redirectUri: `https://${window.location.host}/authresponse`
 });
 window.avs = avs;
@@ -36,7 +36,7 @@ avs.on(AVS.EventTypes.LOGOUT, () => {
 
 avs.on(AVS.EventTypes.TOKEN_INVALID, () => {
   avs.logout()
-  .then(login)
+    .then(login)
 });
 
 avs.on(AVS.EventTypes.LOG, log);
@@ -102,7 +102,7 @@ function logAudioBlob(blob, message) {
     aDownload.download = filename;
     aDownload.textContent = `download`;
 
-    audioLogOutput.innerHTML = `<li>${message}: ${a.outerHTML} ${aDownload.outerHTML}</li>` +audioLogOutput.innerHTML;
+    audioLogOutput.innerHTML = `<li>${message}: ${a.outerHTML} ${aDownload.outerHTML}</li>` + audioLogOutput.innerHTML;
     resolve(blob);
   });
 }
@@ -119,54 +119,55 @@ const playAudio = document.getElementById('playAudio');
 const replayAudio = document.getElementById('replayAudio');
 
 /*
-// If using client secret
-avs.getCodeFromUrl()
+ // If using client secret
+ avs.getCodeFromUrl()
  .then(code => avs.getTokenFromCode(code))
-.then(token => localStorage.setItem('token', token))
-.then(refreshToken => localStorage.setItem('refreshToken', refreshToken))
-.then(() => avs.requestMic())
-.then(() => avs.refreshToken())
-.catch(() => {
+ .then(token => localStorage.setItem('token', token))
+ .then(refreshToken => localStorage.setItem('refreshToken', refreshToken))
+ .then(() => avs.requestMic())
+ .then(() => avs.refreshToken())
+ .catch(() => {
 
-});
-*/
+ });
+ */
 
 avs.getTokenFromUrl()
-.then(() => avs.getToken())
-.then(token => localStorage.setItem('token', token))
-.then(() => avs.requestMic())
-.catch(() => {
-  const cachedToken = localStorage.getItem('token');
+  .then(() => avs.getToken())
+  .then(token => localStorage.setItem('token', token))
+  .then(() => avs.requestMic())
+  .catch(() => {
+    const cachedToken = localStorage.getItem('token');
 
-  if (cachedToken) {
-    avs.setToken(cachedToken);
-    return avs.requestMic();
-  }
-});
+    if (cachedToken) {
+      avs.setToken(cachedToken);
+      return avs.requestMic();
+    }
+  });
 
 loginBtn.addEventListener('click', login);
 
 function login(event) {
   return avs.login()
-  .then(() => avs.requestMic())
-  .catch(() => {});
+    .then(() => avs.requestMic())
+    .catch(() => {
+    });
 
   /*
-  // If using client secret
-  avs.login({responseType: 'code'})
-  .then(() => avs.requestMic())
-  .catch(() => {});
-  */
+   // If using client secret
+   avs.login({responseType: 'code'})
+   .then(() => avs.requestMic())
+   .catch(() => {});
+   */
 }
 
 logoutBtn.addEventListener('click', logout);
 
 function logout() {
   return avs.logout()
-  .then(() => {
-    localStorage.removeItem('token');
-    window.location.hash = '';
-  });
+    .then(() => {
+      localStorage.removeItem('token');
+      window.location.hash = '';
+    });
 }
 
 startRecording.addEventListener('click', () => {
@@ -174,121 +175,129 @@ startRecording.addEventListener('click', () => {
 });
 
 stopRecording.addEventListener('click', () => {
+  console.log(avs);
   avs.stopRecording().then(dataView => {
     avs.player.emptyQueue()
-    .then(() => avs.audioToBlob(dataView))
-    .then(blob => logAudioBlob(blob, 'VOICE'))
-    .then(() => avs.player.enqueue(dataView))
-    .then(() => avs.player.play())
-    .catch(error => {
-      console.error(error);
-    });
+      // .then(() => avs.audioToBlob(dataView))
+      // .then(blob => logAudioBlob(blob, 'VOICE'))
+      // .then(() => avs.player.enqueue(dataView))
+      // .then(() => avs.player.play())
+      .catch(error => {
+        console.error(error);
+      });
 
-        var ab = false;
+    var ab = false;
     //sendBlob(blob);
     avs.sendAudio(dataView)
-    .then(({xhr, response}) => {
+      .then(({xhr, response}) => {
 
-      var promises = [];
-      var audioMap = {};
-      var directives = null;
+        var promises = [];
+        var audioMap = {};
+        var directives = null;
 
-      if (response.multipart.length) {
-        response.multipart.forEach(multipart => {
-          let body = multipart.body;
-          if (multipart.headers && multipart.headers['Content-Type'] === 'application/json') {
-            try {
-              body = JSON.parse(body);
-            } catch(error) {
-              console.error(error);
+        if (response.multipart.length) {
+          response.multipart.forEach(multipart => {
+            console.log(multipart);
+            let body = multipart.body;
+            if (multipart.headers && multipart.headers['Content-Type'] === 'application/json') {
+              try {
+                body = JSON.parse(body);
+
+              } catch (error) {
+                console.error(error);
+              }
+
+              if (body && body.messageBody && body.messageBody.directives) {
+                directives = body.messageBody.directives;
+              }
+            } else if (multipart.headers['Content-Type'] === 'audio/mpeg') {
+              const start = multipart.meta.body.byteOffset.start;
+              const end = multipart.meta.body.byteOffset.end;
+
+              /**
+               * Not sure if bug in buffer module or in http message parser
+               * because it's joining arraybuffers so I have to this to
+               * seperate them out.
+               */
+              var slicedBody = xhr.response.slice(start, end);
+
+              //promises.push(avs.player.enqueue(slicedBody));
+              audioMap[multipart.headers['Content-ID']] = slicedBody;
             }
+          });
 
-            if (body && body.messageBody && body.messageBody.directives) {
-              directives = body.messageBody.directives;
-            }
-          } else if (multipart.headers['Content-Type'] === 'audio/mpeg') {
-            const start = multipart.meta.body.byteOffset.start;
-            const end = multipart.meta.body.byteOffset.end;
-
-            /**
-             * Not sure if bug in buffer module or in http message parser
-             * because it's joining arraybuffers so I have to this to
-             * seperate them out.
-             */
-            var slicedBody = xhr.response.slice(start, end);
-
-            //promises.push(avs.player.enqueue(slicedBody));
-            audioMap[multipart.headers['Content-ID']] = slicedBody;
-          }
-        });
-
-        function findAudioFromContentId(contentId) {
-          contentId = contentId.replace('cid:', '');
-          for (var key in audioMap) {
-            if (key.indexOf(contentId) > -1) {
-              return audioMap[key];
-            }
-          }
-        }
-
-        directives.forEach(directive => {
-          if (directive.namespace === 'SpeechSynthesizer') {
-            if (directive.name === 'speak') {
-              const contentId = directive.payload.audioContent;
-              const audio = findAudioFromContentId(contentId);
-              if (audio) {
-                avs.audioToBlob(audio)
-                .then(blob => logAudioBlob(blob, 'RESPONSE'));
-                promises.push(avs.player.enqueue(audio));
+          function findAudioFromContentId(contentId) {
+            contentId = contentId.replace('cid:', '');
+            for (var key in audioMap) {
+              if (key.indexOf(contentId) > -1) {
+                return audioMap[key];
               }
             }
-          } else if (directive.namespace === 'AudioPlayer') {
-            if (directive.name === 'play') {
-              const streams = directive.payload.audioItem.streams;
-              streams.forEach(stream => {
-                const streamUrl = stream.streamUrl;
+          }
 
-                const audio = findAudioFromContentId(streamUrl);
+          directives.forEach(directive => {
+
+            if (directive.namespace === 'SpeechSynthesizer') {
+              if (directive.name === 'speak') {
+                const contentId = directive.payload.audioContent;
+                const audio = findAudioFromContentId(contentId);
                 if (audio) {
                   avs.audioToBlob(audio)
-                  .then(blob => logAudioBlob(blob, 'RESPONSE'));
+                    .then(blob => logAudioBlob(blob, 'RESPONSE'));
                   promises.push(avs.player.enqueue(audio));
-                } else if (streamUrl.indexOf('http') > -1) {
-                  const xhr = new XMLHttpRequest();
-                  const url = `/parse-m3u?url=${streamUrl.replace(/!.*$/, '')}`;
-                  xhr.open('GET', url, true);
-                  xhr.responseType = 'json';
-                  xhr.onload = (event) => {
-                    const urls = event.currentTarget.response;
-
-                    urls.forEach(url => {
-                      avs.player.enqueue(url);
-                    });
-                  };
-                  xhr.send();
                 }
-              });
-            } else if (directive.namespace === 'SpeechRecognizer') {
-              if (directive.name === 'listen') {
-                const timeout = directive.payload.timeoutIntervalInMillis;
-                // enable mic
+              }
+            } else if (directive.namespace === 'AudioPlayer') {
+              if (directive.name === 'play') {
+                const streams = directive.payload.audioItem.streams;
+                streams.forEach(stream => {
+                  const streamUrl = stream.streamUrl;
+
+                  const audio = findAudioFromContentId(streamUrl);
+                  if (audio) {
+                    avs.audioToBlob(audio)
+                      .then(blob => logAudioBlob(blob, 'RESPONSE'));
+                    promises.push(avs.player.enqueue(audio));
+                  } else if (streamUrl.indexOf('http') > -1) {
+                    const xhr = new XMLHttpRequest();
+                    const url = `/parse-m3u?url=${streamUrl.replace(/!.*$/, '')}`;
+                    xhr.open('GET', url, true);
+                    xhr.responseType = 'json';
+                    xhr.onload = (event) => {
+                      const urls = event.currentTarget.response;
+
+                      urls.forEach(url => {
+                        avs.player.enqueue(url);
+                      });
+                    };
+                    xhr.send();
+                  }
+                });
+              } else if (directive.namespace === 'SpeechRecognizer') {
+                if (directive.name === 'listen') {
+                  const timeout = directive.payload.timeoutIntervalInMillis;
+                  // enable mic
+                }
               }
             }
-          }
-        });
-
-        if (promises.length) {
-          Promise.all(promises)
-         .then(() => {
-            avs.player.playQueue()
           });
-        }
-      }
 
-    })
-    .catch(error => {
-      console.error(error);
-    });
+          if (promises.length) {
+            Promise.all(promises)
+              .then(() => {
+                avs.player.playQueue()
+              })
+              .then(()=> {
+                checkForLocation();
+              });
+          }
+        }
+
+      })
+
+      .catch(error => {
+        console.error(error);
+      });
   });
 });
 
@@ -307,7 +316,18 @@ playAudio.addEventListener('click', (event) => {
 replayAudio.addEventListener('click', (event) => {
   avs.player.replay();
 });
-
+function checkForLocation() {
+  const xhr = new XMLHttpRequest();
+  xhr.open('GET', 'https://thegastro.club/lastevent', true);
+  xhr.responseType = 'json';
+  xhr.onload = (event) => {
+    if (xhr.status = 200) {
+      let resp = xhr.response;
+      console.log(resp);
+    }
+  }
+  xhr.send();
+}
 function sendBlob(blob) {
   const xhr = new XMLHttpRequest();
   const fd = new FormData();
--- example/server.js
+++ example/server.js
@@ -1,36 +1,37 @@
-const https = require('https');
+"use strict";
+//const https = require('https');
 const fs = require('fs');
 const request = require('request');
-
-const options = {
-  // Private Key
-  key: fs.readFileSync('./ssl/server.key'),
-
-  // SSL Certficate
-  cert: fs.readFileSync('./ssl/server.crt'),
-
-  // Make sure an error is not emitted on connection when the server certificate verification against the list of supplied CAs fails.
-  rejectUnauthorized: false
-};
+const http = require('http');
 
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const qs = require('qs');
-const multer  = require('multer');
-const upload = multer({ dest: 'uploads/' });
+const multer = require('multer');
+const upload = multer({dest: 'uploads/'});
 const app = express();
-const port = 9745;
+const cfenv = require('cfenv');
+const appEnv = cfenv.getAppEnv();
+const port = process.env.VCAP_APP_PORT || appEnv.port;
 
-const server = https.createServer(options, app).listen(port, () => {
+process.on('uncaughtException', function (err) {
+  console.log(err);
+});
+const server = http.createServer(app).listen(port, () => {
   console.log(`Express server listening on port ${port}`);
 });
 
-app.use(bodyParser.urlencoded({ extended: true}))
+app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json());
 app.use(cors());
 
 app.use('/', express.static(__dirname));
+
+app.use(function (err, req, res, next) {
+  console.log(err.stack);
+  res.status(500).send('Something broke!');
+});
 
 app.get('/authresponse', (req, res) => {
   res.redirect(301, `/?${qs.stringify(req.query)}`);
@@ -38,6 +39,24 @@ app.get('/authresponse', (req, res) => {
 
 app.post('/audio', upload.single('data'), (req, res) => {
   res.json(req.file);
+});
+
+app.get('/lastevent', (req, res)=> {
+  
+  let MONGO_URL = 'mongodb://hiddengems:test123@aws-us-east-1-portal.17.dblayer.com:10014/hiddengems?ssl=true';
+  let user_id = '1234';
+  let field = "google_place_info";
+  mongodb.MongoClient.connect(MONGO_URL, {ssl: true, sslValidate: false}, function (err, db) {
+    let user_info = db.collections("Users").findOne({"user_id": user_id, "google_place_info": {"$exists": true}});
+    if (user_info) {
+      db.collections("Users")
+        .update({"user_id": user_id, "google_place_info": {"$exists": true}}, {"$unset": {"google_place_info": ""}});
+      res.json({data: user_info.google_place_info});
+    } else {
+      res.json({data: null})
+    }
+  });
+
 });
 
 app.get('/parse-m3u', (req, res) => {
@@ -50,7 +69,7 @@ app.get('/parse-m3u', (req, res) => {
 
   const urls = [];
 
-  request(m3uUrl, function(error, response, bodyResponse) {
+  request(m3uUrl, function (error, response, bodyResponse) {
     console.log(bodyResponse, m3uUrl)
     if (bodyResponse) {
       urls.push(bodyResponse);
@@ -59,3 +78,5 @@ app.get('/parse-m3u', (req, res) => {
     res.json(urls);
   });
 });
+
+
